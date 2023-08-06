e1a43de9589d84c5accd44e35829afdd832e71f7
Vladimir <vlajkic1453@gmail.com>
2023-08-06 22:03:56 +0200

    Tweet Now Links properly to Tweet or User Profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -16,23 +16,31 @@
         </header>
     </x-slot>
     <section class="mt-10">
-        <div class="max-w-sm mx-auto">
+
+        <div class="max-w-sm mx-auto ">
             @foreach ($tweets as $tweet)
-                <header class="flex">
-                    <a href="/{{ $tweet->author }}" class="mr-4">
-                        <h3 class="font-bold">{{ $tweet->author->name }}</h3>
-                    </a>
-                    <a href="/{{ $tweet->author }}" class="mr-4">
-                        {{ '@' . $tweet->author->username }}
-                    </a>
-                    @if ($tweet->created_at->diffInHours(now()) > 24)
-                        <time>{{ $tweet->created_at->format('F j') }}</time>
-                    @else
-                        <time>{{ $tweet->created_at->diffForHumans() }}</time>
-                    @endif
-                </header>
-                <div class="bg-green-300  mb-10">
-                    <p class="bg-green-300">{{ $tweet->body }}</p>
+                <div class="relative">
+                    <a href="status/{{ $tweet->id }}" class="w-full h-full absolute z-0"></a>
+                    <header class="flex">
+                        <a href="/{{ $tweet->author->username }}" class="mr-4 relative hover:underline z-5">
+                            <h3 class="font-bold">{{ $tweet->author->name }}</h3>
+                        </a>
+                        <a href="/{{ $tweet->author->username }}" class="mr-4 relative z-5">
+                            {{ '@' . $tweet->author->username }}
+                        </a>
+                        @if ($tweet->created_at->diffInHours(now()) > 24)
+                            <time>{{ $tweet->created_at->format('F j') }}</time>
+                        @else
+                            <time>{{ $tweet->created_at->diffForHumans() }}</time>
+                        @endif
+                    </header>
+
+                    <div class="bg-green-300  mb-10">
+
+                        <p class="bg-green-300">{{ $tweet->body }}</p>
+
+                    </div>
+
                 </div>
             @endforeach
         </div>
