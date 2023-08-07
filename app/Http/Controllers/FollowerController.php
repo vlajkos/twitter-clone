48ba219d3722131e9fd7bae67dda3758965dcc1e
Vladimir <vlajkic1453@gmail.com>
2023-08-07 22:05:20 +0200

    Users Can't Follow Themselves Now

--- app/Http/Controllers/FollowerController.php
+++ app/Http/Controllers/FollowerController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Follower;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class FollowerController extends Controller
 {
-    //
+    public function store()
+    {
+
+        $follower_id = request()->user()->id;
+        $data = request()->validate(
+            [
+                'following_id' => ['required', 'integer', 'exists:users,id', Rule::unique('followers')->where('follower_id', $follower_id)->whereNot('follower_id', request()->following_id)]
+            ]
+        );
+        $data['follower_id'] = $follower_id;
+
+        Follower::create($data);
+        return back();
+
+
+
+    }
 }
