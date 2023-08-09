01f0a707e5a6656bc464354c70001158eb7fa852
Vladimir <vlajkic1453@gmail.com>
2023-08-09 14:32:13 +0200

    Bio Added

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -22,6 +22,7 @@ class UserFactory extends Factory
             'username' => fake()->userName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
+            'bio' => Str::limit(fake()->paragraph, 160),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
             // password
             'remember_token' => Str::random(10),
